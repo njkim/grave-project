112069d14ae94ab8cc0af726c2e41aaddd7cc6a7
njkim4u <njkim4u@users.noreply.github.com>
2018-12-19 00:49:50 -0800

    sqls updated - alternate name simplified
    alternate name has simplied to just use last name and two sql files (create_tables, input_data) have been updated

--- src/create_tables.sql
+++ src/create_tables.sql
@@ -1,20 +1,21 @@
+CREATE EXTENSION postgis;
+
 CREATE TABLE PERSON (
 person_id				INTEGER	NOT NULL,
 first_name				VARCHAR(15)	NOT NULL,
 last_name 				VARCHAR(15)	NOT NULL,
-alternate_first_name	VARCHAR(15),
-alternate_last_name 	VARCHAR(15),
+alternate_name			VARCHAR(30),
 PRIMARY KEY (person_id)
 );
- 
+
 CREATE TABLE GRAVE (
 grave_id 				INTEGER	NOT NULL,
 lat						REAL NOT NULL,
 lon						REAL NOT NULL,
 geom					geometry(POINT,4326),
 PRIMARY KEY (grave_id),
-CHECK (latitude >= -90.0 AND latitude <= 90.0),
-CHECK (longitude >= -180.0 AND longitude <= 180.0)
+CHECK (lat >= -90.0 AND lat <= 90.0),
+CHECK (lon >= -180.0 AND lon <= 180.0)
 );
 
 CREATE TABLE INTERRED (
@@ -24,7 +25,7 @@ PRIMARY KEY (grave_id, person_id),
 FOREIGN KEY (grave_id) 	REFERENCES grave(grave_id),
 FOREIGN KEY (person_id) REFERENCES PERSON(person_id)
 );
- 
+
 CREATE TABLE RELATIONSHIP (
 person1_id				INTEGER	NOT NULL,
 person2_id				INTEGER	NOT NULL,
